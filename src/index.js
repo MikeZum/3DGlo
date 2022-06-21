6547e68802bf2a184c9f3cd2ae3420e40adc8511
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-21 10:47:33 +0800

    m

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const burger = require("./modules/burger");
+
+// Современный вариант
+import sliderOne from "./modules/slider";
+
+// const sliderOne = require("./modules/slider");
+const sliderTwo = require("./modules/badScript");
+
+burger();
+sliderOne();
+sliderTwo();
---
+++ src/modules/slider.js
@@ -0,0 +1,10 @@
+const sliderModule = () => {
+  const slider = "my slider";
+
+  console.log(slider);
+};
+
+export default sliderModule;
+
+// Старый вариант
+// module.exports = sliderModule;
